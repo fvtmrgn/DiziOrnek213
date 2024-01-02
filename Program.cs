e64b2accd91ec38b92a2987a26eed2626f9e4fea
Fatımatüzzehra Ergen <88490838+fvtmrgn@users.noreply.github.com>
2024-01-02 19:24:52 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiziOrnek213
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, gecici, EnBuyukYer;
+            int[] A = { 78, 56, 23, 95, 68, 87, 61, 77, 45, 33 };
+            for (i = 0; i < A.Length - 1; i++)
+            {
+                EnBuyukYer = i;
+                for (j = i + 1; j < A.Length ; j++)
+                {
+                    if (A[j] > A[EnBuyukYer]) EnBuyukYer = j; 
+                }
+                gecici = A[i]; 
+                A[i] = A[EnBuyukYer];
+                A[EnBuyukYer] = gecici;
+               
+            }
+            for (i = 0; i < A.Length; i++)
+            {
+                Console.WriteLine(A[i]);
+            }
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
